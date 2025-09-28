98a9d93cbd55ed46c812d05a270daf574e9e1121
Christopher Bull <5499680+chrisb13@users.noreply.github.com>
2025-09-28 13:16:56 +1000

    first cody commit of cm3 evaluation repo, note that 00_template_notebook.ipynb is @dougiesquire's work

---
+++ .github/ISSUE_TEMPLATE/add-user-request-to--access-cm3-1-repository-.md
@@ -0,0 +1,18 @@
+---
+name: Add user request to `access-cm3-paper-1` repository.
+about: Use this template to request write access to this repository.
+title: 'User add: '
+labels: 'new_user'
+assignees: 'chrisb13'
+
+---
+
+---- Delete below text ---- 
+
+Welcome! Great to hear you'd like to contribute. :)
+
+(Optional) sentence about your interest and background in being involved in CM3 evaluation.
+
+Lodging this issue will notify an admin to grant you write access. 
+
+Once access is granted, please follow the contribution guidelines in the [`README`](https://github.com/ACCESS-Community-Hub/access-cm3-paper-1).  
---
+++ notebooks/mkfigs.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#PBS -l storage=gdata/tm70+gdata/ik11+gdata/ol01+gdata/xp65
+#PBS -M chris.bull@anu.edu.au
+#PBS -m ae
+#PBS -q normal
+#PBS -W umask=0022
+#PBS -l ncpus=8
+#PBS -l mem=24gb
+#PBS -l walltime=2:00:00
+#PBS -o /g/data/tm70/cyb561/logs
+#PBS -e /g/data/tm70/cyb561/logs
+
+# bash script that runs all the notebooks
+#set -x
+module purge
+module use /g/data/xp65/public/modules
+module load conda/analysis3-25.09 #contains papermill 2.6.0 - https://github.com/ACCESS-NRI/ACCESS-Analysis-Conda/issues/310
+module list
+
+## workflow
+#1. `cd /g/data/tm70/cyb561;git clone git@github.com:ACCESS-Community-Hub/access-cm3-paper-1.git`
+#1. Edit this file and `chmod u+x mkfigs.sh`
+#1. add path to WFOLDER
+#1. set path to ESMDIR (ESM-datastore for experiment)
+#1. ensure the experiment folder is availble in storage header above
+#1. `qsub mkfigs.sh`
+
+## Optional
+#1. change email and log settings in above header
+#1. this script can also be run from an ARE session
+
+
+# SET THESE START
+WFOLDER=/g/data/tm70/cyb561/access-cm3-paper-1/
+ESMDIR=/g/data/ol01/access-om3-output/access-om3-025/MC_25km_jra_ryf-1.0-beta/experiment_datastore.json
+
+# SET THESE END
+
+#best not mess with the path here...
+OFOL=${WFOLDER}notebooks/mkfigs_output4/
+
+cd ${WFOLDER}
+cd notebooks
+mkdir -p ${OFOL}
+
+echo ""
+echo ""
+echo "We are running ALL the notebooks."
+echo "We are using ESMDIR: "${ESMDIR}
+echo "We are using working folder (WFOLDER): "${WFOLDER}
+echo "Output will be in: "${OFOL}
+echo ""
+echo ""
+
+#make the figures
+#array=( 00_template_notebook )
+
+for FNAME in "${array[@]}"
+do
+    echo "Running notebook: "${FNAME}".ipynb"
+    python3 run_nb.py ${FNAME}.ipynb; papermill ${FNAME}.ipynb ${OFOL}${FNAME}_rendered.ipynb -p esm_file ${ESMDIR} -p plotfolder ${OFOL} ; jupyter nbconvert --to markdown ${OFOL}${FNAME}_rendered.ipynb
+done
